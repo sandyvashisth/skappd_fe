f57c02639dfe9019d7c9228fc524496202653b95
Moiz Bookwala <moiz@Moizs-MacBook-Pro.local>
2022-11-17 01:07:11 +0530

    chore: add pm2 options

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [{
+    script: 'npm start'
+  },
+
+  deploy : {
+    production : {
+      key  : 'Keys/Staging.pem'
+      user : 'ubuntu',
+      host : 'ec2-65-2-161-186.ap-south-1.compute.amazonaws.com',
+      ref  : 'origin/api-integration',
+      repo : 'https://github.com/moizmb/skappd_fe.git',
+      path : '/home/ubuntu/apps/skappd',
+      'pre-deploy-local': '',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && yarn && yarn build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
